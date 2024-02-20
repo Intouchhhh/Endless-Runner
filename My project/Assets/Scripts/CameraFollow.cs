d7dd4c1372b7d5bf55e54112767a2562c3260cfc
Intouch Yuso <act22117@gmail.com>
2024-02-21 02:54:01 +0700

    adding smoothfollowcamera and left/right movement

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    public Transform playerTransform; 
-    public Vector3 offset; // adjustable offset from the player (in the inspector)
+    private Vector3 _offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime;
+    private Vector3 _currentVelocity = Vector3.zero;
 
-    void LateUpdate()
+    private void Awake()
     {
-        if (playerTransform != null)// check if player exists to avoid errors
-        {
-            transform.position = playerTransform.position + offset;
-        }
+        _offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
     }
 }
--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpForce = 10.0f;
+    public float dodgeSpeed = 7.5f; 
     private Rigidbody rb;
     private bool isGrounded;
     
@@ -14,18 +15,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Horizontal movement
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        // Horizontal movement (make the player move foward)
         transform.Translate(Vector3.right * speed * Time.deltaTime);
 
-        //Jump 
+        //Jump (space key)
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
-
+        
+        // left right movement using z axis ( A/D key )
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(0.0f, 0.0f, -moveHorizontal);
+        transform.Translate(movement * dodgeSpeed * Time.deltaTime);
+       
     }
 
     void OnCollisionEnter(Collision collision)
